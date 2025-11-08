1d62f99e6975115ca39f8f92514298d221490dc8
WillRIT <144167180+WillRIT@users.noreply.github.com>
2025-11-08 18:55:29 -0500

    Awesome Pick 3 done

--- scenes/ui/pick_three.gd
+++ scenes/ui/pick_three.gd
@@ -1,13 +1,26 @@
 class_name PickThree extends MarginContainer
-
-@onready var buttons: Array[Button] = [%FirstButton, %SecondButton, %ThirdButton]
-
 var player: Player = null
+
+@onready var strengthButt : Button = %FirstButton
+@onready var healthButt : Button = %SecondButton
+@onready var speedButt : Button = %ThirdButton
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
-
-# health speed strength
-func get_upgrades() -> void:
+	strengthButt.pressed.connect(upgrade_chosen.bind("Strength"))
+	healthButt.pressed.connect(upgrade_chosen.bind("Health"))
+	speedButt.pressed.connect(upgrade_chosen.bind("Speed"))
+	strengthButt.grab_focus() 
+	 
 	
+
+func upgrade_chosen(upgrade: String):
+	match(upgrade):
+		"Health":
+			player.increase_stat("health", 1.25)
+		"Strength":
+			player.increase_stat("strength", 1.20)
+		"Speed":
+			player.increase_stat("speed", 1.15)
+			pass
 	pass
