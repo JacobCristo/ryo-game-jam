29a1e9e470df7c930a8ca696e42441909c57fbd9
WillRIT <144167180+WillRIT@users.noreply.github.com>
2025-11-08 10:56:17 -0500

    final changes

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,18 +1,23 @@
 class_name Player extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 600.0
 const JUMP_VELOCITY = -400.0
 
+var dash_scalar: float = 200.0
+var dash_length: float = 0.1
 var health: float = 100.0
 
-func _physics_process(_delta: float) -> void:
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+#TODO 	Dash Cooldown Timer 
 
+
+func _physics_process(_delta: float) -> void:
+	var direction := Input.get_vector("move_left","move_right","move_up","move_down")
+	print(direction)
+	velocity = direction * SPEED
 	move_and_slide()
+	
+	if (Input.is_action_pressed("dash")):
+		dash(direction)
 
 func take_damage(damage: float) -> void:
 	health -= damage
@@ -22,3 +27,14 @@ func take_damage(damage: float) -> void:
 func die() -> void:
 	queue_free()
 	print("you died loser")
+	
+	
+
+func dash(dir: Vector2) -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "position", Vector2(global_position + (dir * dash_scalar)), dash_length)
+	pass
+	
+	
+	
+	
