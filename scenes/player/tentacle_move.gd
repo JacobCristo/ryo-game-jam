472dff97b8fe5c76e896232480e93c64f0ed3790
zxjordan5 <zxjordan5@gmail.com>
2025-11-08 10:17:14 -0500

    Finished 1st draft movement script

--- scenes/player/tentacle_move.gd
+++ scenes/player/tentacle_move.gd
@@ -1,9 +1,12 @@
 extends Node
 
-# TODO: Access rigidbody on the thing with CTRL + DRAG
+# TODO: Test script
+@onready var _r_body: RigidBody2D = $TentacleBase/End
 
 var _input_dir
+var _force
 const DEADZONE = 0.1
+const FORCE_AMT = 20
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,7 +19,6 @@ func _process(delta: float) -> void:
 	_read_input()
 	_apply_force_to_chain()
 	
-	
 func _read_input() -> void:
 	# Read input and register to unit vector variable
 	_input_dir = Input.get_vector(
@@ -27,5 +29,8 @@ func _read_input() -> void:
 		DEADZONE).normalized()
 	
 func _apply_force_to_chain() -> void:
-	pass
+	# Multiply direction by FORCE_AMT
+	_force = _input_dir * FORCE_AMT
+	_r_body.apply_central_force(_force)
+	
 	
