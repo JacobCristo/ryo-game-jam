0c8d9815e14c8d0cde35b2d178c45986403489e4
WillRIT <144167180+WillRIT@users.noreply.github.com>
2025-11-08 11:46:53 -0500

    Merge branch 'main' into features/pick-3

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,24 +3,39 @@ class_name Player extends CharacterBody2D
 @onready var music_bus_index = AudioServer.get_bus_index("Music")
 @onready var low_pass_filter = AudioServer.get_bus_effect(music_bus_index, 0)
 
-const SPEED = 600.0
-const JUMP_VELOCITY = -400.0
+@export_group("Dash Stats")
+@export var dash_cooldown: float = 1.0#s
+@export var dash_scalar: float = 2500.0
+@export var dash_length: float = 0.1
 
-var dash_scalar: float = 200.0
-var dash_length: float = 0.1
-var health: float = 100.0
+@export_group("Player Stats")
+@export var speed: float = 600.0
+@export var strength: float = 20.0
+@export var health: float = 100.0
 
-#TODO 	Dash Cooldown Timer 
+var dash_timer: float = 0.0
+var is_dashing: float = false #bro so ugly
 
-
-func _physics_process(_delta: float) -> void:
-	var direction := Input.get_vector("move_left","move_right","move_up","move_down")
-	print(direction)
-	velocity = direction * SPEED
+func _physics_process(delta: float) -> void:
+	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	if not is_dashing:
+		velocity = direction * speed
+	
 	move_and_slide()
 	
-	if (Input.is_action_pressed("dash")):
-		dash(direction)
+	dash_timer = max(dash_timer - delta, 0.0)
+	if Input.is_action_pressed("dash") and dash_timer == 0.0:
+		start_dash(direction)
+		dash_timer = dash_cooldown
+
+func start_dash(dir: Vector2) -> void:
+	if dir == Vector2.ZERO:
+		return
+	is_dashing = true
+	velocity = dir.normalized() * dash_scalar
+	await get_tree().create_timer(dash_length).timeout
+	is_dashing = false
 
 func take_damage(damage: float) -> void:
 	health -= damage
@@ -43,14 +58,23 @@ func apply_damage_effect():
 
 func _restore_music():
 	create_tween().tween_property(low_pass_filter, "cutoff_hz", 2000, 1).set_ease(Tween.EASE_IN)
-	
-	
 
 func dash(dir: Vector2) -> void:
-	var tween = get_tree().create_tween()
-	tween.tween_property(self, "position", Vector2(global_position + (dir * dash_scalar)), dash_length)
-	pass
+	if dir == Vector2.ZERO:
+		return
+
+	velocity = dir.normalized() * dash_scalar
+	move_and_slide()
 	
-	
-	
-	
+	# brief dash duration
+	await get_tree().create_timer(dash_length).timeout
+	velocity = Vector2.ZERO
+
+func increase_stat(stat_name: String, increase: int) -> void:
+	match stat_name.to_lower():
+		"health":
+			health += increase
+		"speed": 
+			speed += increase
+		"strength":
+			strength += increase
