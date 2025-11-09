3405f431d38a35b38b0926d13f1d1eb09d5fc0cc
zxjordan5 <zxjordan5@gmail.com>
2025-11-08 20:57:45 -0500

    Debugging physics

--- scenes/enemies/enemy.gd
+++ scenes/enemies/enemy.gd
@@ -22,7 +22,7 @@ const VELOCITY_DMG_SCALER: float = 100.0
 var health: float
 var stunned_cool_down: float
 var state: EnemyState
-var stunned_dir: Vector2
+var _stunned_dir: Vector2
 
 enum EnemyState {
 	ACTIVE,
@@ -32,6 +32,8 @@ enum EnemyState {
 
 func _ready() -> void:
 	health = max_health
+	_stunned_dir = Vector2.ZERO
+	
 	# set raycast length to sightline range
 	sightline.target_position.x = sightline_range
 	
@@ -53,6 +55,7 @@ func _process(delta: float) -> void:
 		if(stunned_cool_down <= 0) :
 			state = EnemyState.ACTIVE
 			stunned_cool_down = STUNNED_COOLDOWN_MAX
+			velocity = Vector2.ZERO
 			
 			
 		
@@ -63,7 +66,7 @@ func _physics_process(delta: float) -> void:
 		_stunned_physics_process(delta)
 		
 func _stunned_physics_process(delta: float) -> void:
-	velocity = stunned_dir * stunned_speed * delta
+	velocity = _stunned_dir * stunned_speed * delta
 	move_and_slide()
 	
 	
@@ -129,39 +132,47 @@ func _determine_correct_animation() :
 
 func _on_hitbox_body_entered(body: Node2D) -> void:
 	# if stunned return
+	print("BODY: " + body.name)
 	if(state == EnemyState.STUNNED) :
-		return 
-		
-	# if body is Tentacle or whatever:
-		# take_damage(x amount of damage)
-		# if health is 0 activate die
-		# might cause issues with reading in class
-	var _dmg_amt: float = 0.0
-	if(body is Segment) :
-		
-		pass
+		print("STUNNED")
+		return
 	
-	if(body is TentacleEnd) :
-		pass
-	# Take damage
-	# Apply force
-	# Change state
-	# Change sprite
-	pass
+	if(body is Segment or body is TentacleEnd) :
+		state = EnemyState.STUNNED 
+			
+		var _dmg_amt: float = 0.0
+		if(body is Segment) :
+			body = body as Segment
+			_dmg_amt = _calc_dmg_amt_segment(body)
+		
+		if(body is TentacleEnd) :
+			body = body as TentacleEnd
+			_dmg_amt = _calc_dmg_amt_end(body)
+		
+		# Take damage
+		take_damage(_dmg_amt)
+		
+		# Calculate stun direction
+		var r_body = body.get_parent()
+		if(r_body is RigidBody2D) :
+			_stunned_dir = r_body.linear_velocity.normalized()
+	
 	
 func _calc_dmg_amt_segment(body: Node2D) -> float:
 	var dmg_amt: float = 0.0
-	body = body as Segment
 	
 	var r_body = body.get_parent()
 	if(r_body is RigidBody2D) :
-		dmg_amt = r_body.linear_velocity
+		dmg_amt = r_body.linear_velocity.length() / VELOCITY_DMG_SCALER
 	
 	return dmg_amt
 	
 func _calc_dmg_amt_end(body: Node2D) -> float:
 	var dmg_amt: float = 0.0
-	body = body as TentacleEnd
+	
+	var r_body = body.get_parent()
+	if(r_body is RigidBody2D) :
+		dmg_amt = (r_body.linear_velocity.length() / VELOCITY_DMG_SCALER) * END_DMG_MULT
 	
 	return dmg_amt
 	
